9cf960de3a89f93bc6f57b8d95b7558c584d7c16
Daria Zhdanok <knemune@gmail.com>
2026-02-01 12:18:17 +0300

    Добавлен бэк в репозиторий

---
+++ EWA_backend/src/main/java/com/example/EWA_backend/EwaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EWA_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwaBackendApplication.class, args);
+	}
+
+}
