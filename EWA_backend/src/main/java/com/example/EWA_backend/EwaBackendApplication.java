a4fe1bb8fa16627d4ec5fc8655a7a66777fe9f9d
Daria Zhdanok <knemune@gmail.com>
2026-04-11 00:30:52 +0300

    Update backend

--- EWA_backend/src/main/java/com/example/EWA_backend/EwaBackendApplication.java
+++ EWA_backend/src/main/java/com/example/EWA_backend/EwaBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EwaBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EwaBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EwaBackendApplication.class, args);
+    }
 }
